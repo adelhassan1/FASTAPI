d3f6f7445519539f8fbf2e5f6a5afcd8ad98d148
adelhassan1 <tomasander12@gmail.com>
2024-03-11 15:03:20 +0200

    [updated]

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,3 +1,4 @@
+from turtle import mode, pos
 from .. import models, schemas, oauth2
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
@@ -16,8 +17,10 @@ def get_posts(db: Session = Depends(get_db),
     # cursor.execute("""SELECT * FROM posts """)
     # posts = cursor.fetchall()
 
-    post = db.query(models.Post).all()
-    return post
+    posts = db.query(models.Post).all()
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+
+    return posts
 
 
 
@@ -30,7 +33,6 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
     # new_post = cursor.fetchone()
     # conn.commit()
 
-    print(current_user)
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
@@ -52,6 +54,11 @@ def get_post(id: int, db: Session = Depends(get_db),
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
+    
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+    #                         detail="Not authorized to perform request action")
+    
     return post
 
 
@@ -63,11 +70,18 @@ def delete_post(id: int, db: Session = Depends(get_db),
     # deleted_post = cursor.fetchone()
     # conn.commit()
 
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    post.delete(synchronize_session=False)
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform request action")
+    
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -75,7 +89,7 @@ def delete_post(id: int, db: Session = Depends(get_db),
 
 @router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
     #                 (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
@@ -87,6 +101,10 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     if post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform request action")
     
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
